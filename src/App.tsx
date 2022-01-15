7833728c1338ca95c1f35f674ee49288c0b019cc
Alexei Semenenko <aleksei.syemyenenko@gmail.com>
2022-01-15 16:32:39 +0300

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,50 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
+// @ts-ignore
 import * as THREE from 'three'
 
 import './App.css'
 
 function App() {
-  const scene = new THREE.Scene();
-  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  const mountRef = useRef(null);
 
-  const renderer = new THREE.WebGLRenderer();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  document.body.appendChild(renderer.domElement);
+  useEffect(
+    () => {
+      const scene = new THREE.Scene();
+      const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-  const geometry = new THREE.BoxGeometry();
-  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-  const cube = new THREE.Mesh(geometry, material);
-  scene.add(cube);
+      const renderer = new THREE.WebGLRenderer();
+      renderer.setSize(window.innerWidth, window.innerHeight);
 
-  camera.position.z = 5;
+      // @ts-ignore
+      mountRef.current.appendChild(renderer.domElement);
 
-  function animate() {
-    requestAnimationFrame(animate);
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    renderer.render(scene, camera);
-  }
-  animate();
+      const geometry = new THREE.BoxGeometry();
+      const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+      const cube = new THREE.Mesh(geometry, material);
+
+      scene.add(cube);
+      camera.position.z = 5;
+
+      function animate(): void {
+        requestAnimationFrame(animate);
+        cube.rotation.x += 0.1;
+        cube.rotation.y -= 0.05;
+        cube.rotation.z += 0.01;
+        renderer.render(scene, camera);
+      }
+
+      animate()
+
+      // @ts-ignore
+      return () => mountRef.current.removeChild( renderer.domElement);
+    },
+    []
+  )
 
   return (
-    <p>Денис лох</p>
+    <div ref={mountRef}>
+
+    </div>
   )
 }
 
